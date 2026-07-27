a3557d208535ca5fb7ab17c066d4150eaa0af1fb
vidhansharmaaa2008-maker <vidhan.sharmaaa2008@gmail.com>
2026-07-27 13:42:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,6 +82,7 @@ def prompt_generator(agent):
   with open(file_name, 'w') as f:
     f.write(response.content[-1]['text'])
   return "Prompt file generated successfully, agent can read it"
+    
 prompt_generator(model)
 
 # tool 2
