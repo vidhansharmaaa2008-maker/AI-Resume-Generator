5da4a912c8811016f4396ab7c9896e2c11fd84a9
vidhansharmaaa2008-maker <vidhan.sharmaaa2008@gmail.com>
2026-07-28 12:49:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,4 @@ if st.button('generate resume'):
 
     
       
-    response = agent.invoke({'messages': [{'role':'user','content':query}]})
-    code=response['messages'][-1].content[-1]['text']
-    #st.markdown(code)
     st.html(code , width="stretch" , unsafe_allow_javascript=True)
